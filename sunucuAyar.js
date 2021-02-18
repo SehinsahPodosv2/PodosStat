217036c651b45a0b20a85220fcf38e8ece89ff5f
SehinsahPodosv2 <79246898+SehinsahPodosv2@users.noreply.github.com>
2021-02-18 10:27:42 +0300

    Add files via upload

---
+++ sunucuAyar.js
@@ -0,0 +1,13 @@
+const ayarlar = {
+  guildID: "809285603367190588", // sunucu idsi
+  ownerRole:"809285603749265429", // sunucu sahiplerinin rolü
+  teyitsizRolleri:[""], // kayıt olmayanlara verieln rol
+  jailRolu:"", // cezalılara verilen rol
+  enAltYetkiliRolu:[''], // ilk yetkili permi
+  activity:"Podos💛Şehinşah", // botun durumu
+  status:"online",
+  invitelink:"https://discord.gg/N8KyCQH27e", //davet linki
+  symbol:"Ψ", // tag
+  voicechannel:"809285605988630547" // botun gireceği ses kanalı
+};
+module.exports = ayarlar;
